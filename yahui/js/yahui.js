de97784180cb39b18a2fdbacc3172d2969d92dd6
hobbyquaker <hobbyquaker@gmail.com>
2013-09-01 14:34:34 +0200

    fixed iframe height (usage of css3 calc)

--- yahui/js/yahui.js
+++ yahui/js/yahui.js
@@ -14,7 +14,7 @@
 
 
 var yahui = {
-    version: "0.9.11",
+    version: "0.9.12",
     prefix: "",
     images: [],
     sortOrder: {},
@@ -300,7 +300,7 @@ $(document).ready(function () {
             '<a href="#" id="refresh_'+pageId+'" data-role="button" data-inline="true" data-icon="refresh" data-iconpos="notext" class="yahui-info ui-btn-right"></a>' +
             '<h1>' + text + '</h1>' +
             '</div><div style="margin:0;padding:0;min-height:90%" data-role="content">' +
-            '<iframe style="position:absolute; top:0px; left:0px; width:100%; height:100%; padding-top:42px; border: none;" src="'+src+'" id="if_'+pageId+'"></iframe></div></div>';
+            '<iframe style="position:absolute; top:0px; left:0px; width:100%; height:100%; height:calc(100% - 42px); padding-top:42px; border: none;" src="'+src+'" id="if_'+pageId+'"></iframe></div></div>';
         body.prepend(page);
         $("#refresh_"+pageId).click(function () {
            $("#if_"+pageId).attr('src', function ( i, val ) { return val; });

450a6d1a736d3d28ca424de2ef7f25ae822c3cc0
hobbyquaker <hobbyquaker@gmail.com>
2013-09-21 14:41:37 +0200

    Version 1.0.0 Release

---
+++ yahui/js/settings-dist.js
@@ -0,0 +1,38 @@
+/**
+ *
+ *      yahui Konfiguration
+ *
+ */
+
+var settings = {
+
+    // Hier kann getrennt für die Kopfzeile, den Inhalt und das Menü ein jquerymobile "Swatch" gewählt werden.
+    // a = Dunkelgrau, b=Hellgrau/Blau, c=Hellgrau, d=Grau, e=Gelb
+    // komplett individuelle Themes können hier erstellt werden: http://jquerymobile.com/themeroller/index.php
+    swatches: {
+
+        // Kopfzeile
+        header: "b",
+
+        // Inhalt
+        content: "c",
+
+        // Menü
+        footer: "a"
+    },
+
+    // Info-Button oben rechts ausblenden?
+    hideInfoButton: false,
+
+    // Bestimmte Datenpunkte von CUxD Geräten ausblenden (_MIN_ betrifft auch _MAX_)
+    hideDatapoints: {
+        TEMP_MIN_24H: false,
+        ABS_HUMIDITY: false,
+        HUM_MIN_24H: false,
+        DEWPOINT: false,
+        MISS_24H: true,
+        COUNTER: true,
+        SUM: true,
+        LOWBAT: true
+    }
+};
